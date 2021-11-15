496cbe56ac75a576083d30328e3ce01e48e16784
Meni <morenbach@nvidia.com>
2021-11-15 07:24:13 +0000

    tracer host side for invoking the pta

---
+++ tracer/Android.mk
@@ -0,0 +1,14 @@
+###################### optee-tracer ######################
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_CFLAGS += -DANDROID_BUILD
+LOCAL_CFLAGS += -Wall
+
+LOCAL_SRC_FILES += host/main.c
+
+LOCAL_SHARED_LIBRARIES := libteec
+LOCAL_MODULE := optee_tracer
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
